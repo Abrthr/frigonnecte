ae33697df52d095af28320953d475a63a5ceec6e
Abrthr <aurelienberthier78@gmail.com>
2022-01-27 16:00:52 +0100

    Création des fichiers urls.py

---
+++ LogIn/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include,path
+from django.contrib.auth.views import (LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView, AuthenticationForm, PasswordChangeForm, PasswordResetForm)
+
+app_name = 'LogIn'
+
+urlpatterns = [
+    path('', include('django.contrib.auth.urls'))
+]
