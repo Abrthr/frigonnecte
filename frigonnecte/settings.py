6ca67a31bcb6f7b46c9f83215922346b5f0de5d9
Florian <98222127+FlorianBiboud@users.noreply.github.com>
2022-02-03 15:46:30 +0100

    création de la database
    création d'une database sqlit3

--- frigonnecte/settings.py
+++ frigonnecte/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'frigonnecte.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'frigo_database',
     }
 }
 
