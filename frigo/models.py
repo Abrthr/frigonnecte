e15642c639e8db6e943273662a6090f388b075f8
Florian <98222127+FlorianBiboud@users.noreply.github.com>
2022-02-10 16:22:46 +0100

    la base de donnée est créé avec les tables et les premiers labels
    j'espère qu'il ne va pas trop falloir tout modifier ^^

--- frigo/models.py
+++ frigo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from recette.models import Recette,Ingredient
+
 # Create your models here.
+
+class Frigo(models.Model):
+    nom_frigo = models.CharField(max_length=50)
+
+class contient (models.Model):
+    frigo = models.ForeignKey(Frigo,on_delete=models.CASCADE)
+    recette = models.ForeignKey(Recette,on_delete=models.PROTECT)
+    ingredient = models.ForeignKey(Ingredient,on_delete=models.PROTECT)
+    quantite = models.FloatField()
+    date_achat = models.DateField(auto_now=True)
+    date_de_peremption = models.DateField()
+
